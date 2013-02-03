59bc6b94b318d50c3e241a2ab2eb76dfa8884263
Prathamesh Sonpatki <csonpatki@gmail.com>
2013-02-03 22:50:38 +0530

    Completed upto exercise 1.3

---
+++ chapter_1/1-3.scm
@@ -0,0 +1,17 @@
+; Define a procedure that takes three numbers as arguments and returns
+; the sum of squares of the two larger numbers
+
+(define (square x) (* x x))
+; (square 6) 36
+
+(define (sum-of-squares x y) (+ (square x) (square y)))
+; (sum-of-squares 3 4) 25
+
+(define (max x y) (if (> x y) x y))
+; (max 3 4) 4
+
+(define (sum-of-squares-of-2-larger x y z)
+        (sum-of-squares (max x y) (max y z))
+)
+; (sum-of-squares-of-2-larger 1 2 3) 13
+; (sum-of-squares-of-2-larger 5 4 3) 41

4412efa543ec2370fbbf8add8e31b4e14044f19f
Prathamesh Sonpatki <csonpatki@gmail.com>
2013-02-07 22:35:22 +0530

    Boilerplate for the-little-schemer

---
+++ the-little-schemer/tls.scm
@@ -0,0 +1,16 @@
+;; The Little Schemer uses these 3 functions extensively
+;; I don't like to type them all the time
+;; load('tls.scm')
+;; Enjoy!
+
+(define atom?
+  (lambda (x)
+    (and (not (pair? x)) (not (null? x)))))
+
+(define add1
+  (lambda (x)
+    (+ x 1)))
+
+(define sub1
+  (lambda (x)
+    (- x 1)))

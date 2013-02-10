f67faa34895201602f46264f30bbdac27dad6cdb
Prathamesh Sonpatki <csonpatki@gmail.com>
2013-02-10 23:10:16 +0530

    Added a better solution for Exercise 1-3

--- chapter_1/1-3.scm
+++ chapter_1/1-3.scm
@@ -20,3 +20,19 @@
 ; (sum-of-squares-of-2-larger 1 2 3) 13
 ; (sum-of-squares-of-2-larger 5 4 3) 41
 ; (sum-of-squares-of-2-larger 1 3 2) 13
+
+;;
+;; Another approach (Better)
+;;
+
+(define (min x y) (if (< x y) x y))
+; (min 10 5) 5
+
+(define (sum-of-squares-of-2-larger x y z)
+        (sum-of-squares (max x y)
+                        (max (min x y) z)
+                        ))
+
+; (sum-of-squares-of-2-larger 1 2 3) 13
+; (sum-of-squares-of-2-larger 5 4 3) 41
+; (sum-of-squares-of-2-larger 1 3 2) 13
